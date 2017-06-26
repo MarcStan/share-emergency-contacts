317a3e0f265d7a458e14a8a5c215806a1673b2d1
MarcStan <marcstan@live.com>
2017-06-26 12:44:35 +0200

    manuell view first approach works on all platforms

---
+++ src/ShareEmergencyContacts/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace ShareEmergencyContacts.ViewModels
+{
+    class MainViewModel
+    {
+        public string Message => $"Welcome to Xamarin Forms on {Device.RuntimePlatform}!";
+    }
+}
