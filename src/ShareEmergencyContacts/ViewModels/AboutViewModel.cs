9548975a2133358ad4a85dd678ec57aca91b0cf8
MarcStan <marcstan@live.com>
2017-06-30 19:09:13 +0200

    removed close button

--- src/ShareEmergencyContacts/ViewModels/AboutViewModel.cs
+++ src/ShareEmergencyContacts/ViewModels/AboutViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using Caliburn.Micro;
-using Caliburn.Micro.Xamarin.Forms;
-using System.Windows.Input;
-using Xamarin.Forms;
 
 namespace ShareEmergencyContacts.ViewModels
 {
@@ -10,19 +7,11 @@ namespace ShareEmergencyContacts.ViewModels
         public AboutViewModel()
         {
             Version = $"Share emergency contacts v{IoC.Get<IAppInfoProvider>().UserFriendlyVersion}";
-            CloseCommand = new Command(Close);
         }
-
-        public ICommand CloseCommand { get; }
 
         /// <summary>
         /// Returns the current version
         /// </summary>
         public string Version { get; }
-
-        public void Close()
-        {
-            IoC.Get<INavigationService>().GoBackAsync();
-        }
     }
 }
