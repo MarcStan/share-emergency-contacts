a59a8e5bc1871b67d0875060f0f83a4a34f14346
MarcStan <marcstan@live.com>
2017-06-30 19:09:00 +0200

    fixed android styled buttons looking different than on other platforms

---
+++ src/ShareEmergencyContacts.Android/CustomButtonRenderer.cs
@@ -0,0 +1,16 @@
+﻿using ShareEmergencyContacts.Droid.Express.CustomRenderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomButtonRenderer))]
+namespace ShareEmergencyContacts.Droid
+{
+    namespace Express.CustomRenderers
+    {
+        /// <summary>
+        /// Workaround for android button styling not working ala https://stackoverflow.com/a/38455653
+        /// </summary>
+        public class CustomButtonRenderer : Xamarin.Forms.Platform.Android.ButtonRenderer
+        {
+        }
+    }
+}
