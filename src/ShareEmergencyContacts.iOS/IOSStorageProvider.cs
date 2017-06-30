c728ab5184b1f6d93e15149d4bb26d0725fe11a6
MarcStan <marcstan@live.com>
2017-06-30 18:30:42 +0200

    fixed iOS

--- src/ShareEmergencyContacts.iOS/IOSStorageProvider.cs
+++ src/ShareEmergencyContacts.iOS/IOSStorageProvider.cs
@@ -1,17 +1,52 @@
-﻿using ShareEmergencyContacts.Models;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using ShareEmergencyContacts.Models;
 
 namespace ShareEmergencyContacts.iOS
 {
     public class IOSStorageProvider : IStorageProvider
     {
-        public void WriteAllText(string filePath, string text)
+        public async Task WriteAllTextAsync(string filePath, string text)
         {
-            throw new System.NotImplementedException();
+            await Task.Run(() =>
+            {
+                var fi = new FileInfo(filePath);
+                if (!Directory.Exists(fi.DirectoryName))
+                    Directory.CreateDirectory(fi.DirectoryName);
+                File.WriteAllText(filePath, text);
+            });
         }
 
-        public string ReadAllText(string filePath)
+        public async Task<string> ReadAllTextAsync(string filePath)
         {
-            throw new System.NotImplementedException();
+            return await Task.Run(() => File.ReadAllText(filePath));
+        }
+
+        public async Task<IReadOnlyList<string>> GetFilesAsync(string directory, string pattern = null)
+        {
+            if (!Directory.Exists(directory))
+                return new List<string>();
+
+            return await Task.Run(() =>
+            {
+                var files = Directory.GetFiles(directory, pattern);
+                return files;
+            });
+        }
+
+        public async Task<IReadOnlyList<string>> ReadAllLinesAsync(string filePath)
+        {
+            return await Task.Run(() => File.ReadAllLines(filePath));
+        }
+
+        public async Task DeleteFileAsync(string filePath)
+        {
+            await Task.Run(() =>
+            {
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+            });
         }
     }
 }
