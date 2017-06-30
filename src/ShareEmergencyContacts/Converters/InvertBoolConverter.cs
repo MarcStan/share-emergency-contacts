933fcfc113516846ae30f4cdbb5c96c12826b406
MarcStan <marcstan@live.com>
2017-06-30 18:31:04 +0200

    forcing list collapse because iOS does not collapse empty lists by default

---
+++ src/ShareEmergencyContacts/Converters/InvertBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ShareEmergencyContacts.Converters
+{
+    public class InvertBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+                return !(bool)value;
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
