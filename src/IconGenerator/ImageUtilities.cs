81b329b16a9abe7e74500f026eb1041639c0773e
MarcStan <marcstan@live.com>
2017-07-18 14:54:36 +0200

    fixed memory leak

--- src/IconGenerator/ImageUtilities.cs
+++ src/IconGenerator/ImageUtilities.cs
@@ -25,40 +25,48 @@ namespace IconGenerator
         public static void ResizeImage(string file, string target, int width, int height, bool maintainAspectRatio)
         {
             var image = Image.FromFile(file);
-            //a holder for the result
             Bitmap result = new Bitmap(width, height);
-            //set the resolutions the same to avoid cropping due to resolution differences
-            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            //use a graphics object to draw the resized image into the bitmap
-            using (Graphics graphics = Graphics.FromImage(result))
+            try
             {
-                //set the resize quality modes to high quality
-                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                //set the resolutions the same to avoid cropping due to resolution differences
+                result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
 
-                //draw the image into the target bitmap
-                if (maintainAspectRatio)
+                //use a graphics object to draw the resized image into the bitmap
+                using (Graphics graphics = Graphics.FromImage(result))
                 {
-                    var minSize = Math.Min(result.Height, result.Width);
-                    var sourceAspect = (float)Math.Max(image.Width, image.Height) / Math.Min(image.Width, image.Height);
-                    var maxSize = minSize * sourceAspect;
-                    var xSize = sourceAspect >= 1 ? maxSize : minSize;
-                    var ySize = sourceAspect >= 1 ? minSize : maxSize;
-                    var offsetX = (result.Width - xSize) / 2;
-                    var offsetY = (result.Height - ySize) / 2;
-                    graphics.DrawImage(image, offsetX, offsetY, xSize, ySize);
-                }
-                else
-                {
-                    graphics.DrawImage(image, 0, 0, result.Width, result.Height);
-                }
+                    //set the resize quality modes to high quality
+                    graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 
+                    //draw the image into the target bitmap
+                    if (maintainAspectRatio)
+                    {
+                        var minSize = Math.Min(result.Height, result.Width);
+                        var sourceAspect = (float)Math.Max(image.Width, image.Height) /
+                                           Math.Min(image.Width, image.Height);
+                        var maxSize = minSize * sourceAspect;
+                        var xSize = sourceAspect >= 1 ? maxSize : minSize;
+                        var ySize = sourceAspect >= 1 ? minSize : maxSize;
+                        var offsetX = (result.Width - xSize) / 2;
+                        var offsetY = (result.Height - ySize) / 2;
+                        graphics.DrawImage(image, offsetX, offsetY, xSize, ySize);
+                    }
+                    else
+                    {
+                        graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+                    }
+
+                }
+                var dir = new FileInfo(target).DirectoryName;
+                Directory.CreateDirectory(dir);
+                result.Save(target, ImageFormat.Png);
             }
-            var dir = new FileInfo(target).DirectoryName;
-            Directory.CreateDirectory(dir);
-            result.Save(target, ImageFormat.Png);
+            finally
+            {
+                image.Dispose();
+                result.Dispose();
+            }
         }
     }
 }
