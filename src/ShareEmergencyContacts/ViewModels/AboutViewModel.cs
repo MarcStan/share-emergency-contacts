bdabd1629bcdb39b011155399c32771a86e580aa
MarcStan <marcstan@live.com>
2017-07-18 15:57:09 +0200

    updated about screen

--- src/ShareEmergencyContacts/ViewModels/AboutViewModel.cs
+++ src/ShareEmergencyContacts/ViewModels/AboutViewModel.cs
@@ -8,19 +8,42 @@ namespace ShareEmergencyContacts.ViewModels
 {
     public class AboutViewModel : Screen
     {
+        private BindableCollection<string> _poweredBy;
+        private const string Url = "https://marcstan.net/betas/SEC";
+
         public AboutViewModel()
         {
             Version = $"{GetType().GetTypeInfo().Assembly.GetName().Version.ToString(3)}";
             WebsiteCommand = new Command(() =>
             {
-                Device.OpenUri(new Uri("https://marcstan.net/"));
+                Device.OpenUri(new Uri(Url));
             });
 
             ShareBetaCommand = new Command(() =>
             {
                 var share = IoC.Get<IShareProvider>();
-                share.ShareUrl("https://marcstan.net/betas/SEC/", "Download \"Share emergency contacts\"", "Check out the app \"Share emergency contacts\" (currently in beta)");
+                share.ShareUrl(Url, "Download \"Share emergency contacts\"", "Check out the app \"Share emergency contacts\" (currently in beta)");
             });
+
+            PoweredBy = new BindableCollection<string>
+            {
+                "Xamarin.Forms",
+                "XLabs",
+                "Caliburn.Micro",
+                "Acr UserDialogs",
+                "ZXing .Net Mobile Forms"
+            };
+        }
+
+        public BindableCollection<string> PoweredBy
+        {
+            get => _poweredBy;
+            set
+            {
+                if (Equals(value, _poweredBy)) return;
+                _poweredBy = value;
+                NotifyOfPropertyChange(nameof(PoweredBy));
+            }
         }
 
         /// <summary>
