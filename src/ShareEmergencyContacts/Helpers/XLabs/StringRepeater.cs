cceaeff23a725acc60243c17b07be68b744d003b
MarcStan <marcstan@live.com>
2017-07-19 13:39:19 +0200

    workaround for iOS crashing with generic types in xaml

---
+++ src/ShareEmergencyContacts/Helpers/XLabs/StringRepeater.cs
@@ -0,0 +1,16 @@
+﻿using XLabs.Forms.Controls;
+
+namespace ShareEmergencyContacts.Helpers.XLabs
+{
+    /// <summary>
+    /// iOS doesn't like generic types in xaml (instant crash on page load).
+    /// Thus derive it manually for each type.
+    /// </summary>
+    /// <remarks>
+    /// Originally found solution at: https://stackoverflow.com/questions/26536000/repeaterviewt-in-xamarin-forms-labs
+    /// </remarks>
+    public class StringRepeater : RepeaterView<string>
+    {
+
+    }
+}
