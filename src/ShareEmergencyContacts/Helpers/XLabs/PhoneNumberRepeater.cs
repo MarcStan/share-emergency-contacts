d03232cd8e441e4c801de91e1cccd6132f3b4d31
MarcStan <marcstan@live.com>
2017-07-20 00:13:12 +0200

    more fixes for iOS

---
+++ src/ShareEmergencyContacts/Helpers/XLabs/PhoneNumberRepeater.cs
@@ -0,0 +1,11 @@
+﻿using ShareEmergencyContacts.ViewModels.ForModels;
+using XLabs.Forms.Controls;
+
+namespace ShareEmergencyContacts.Helpers.XLabs
+{
+    public class PhoneNumberRepeater : RepeaterView<PhoneNumberViewModel>
+
+    {
+
+    }
+}
