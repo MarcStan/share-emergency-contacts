825c6b2f90120ff6f7f2acd129b7d2d5175e8233
MarcStan <marcstan@live.com>
2017-07-19 20:53:26 +0200

    fix to display proper error on view not found causes views to be instantiated twice

--- src/ShareEmergencyContacts/App.xaml.cs
+++ src/ShareEmergencyContacts/App.xaml.cs
@@ -42,20 +42,6 @@ namespace ShareEmergencyContacts
             var storageProvider = IoC.Get<IStorageProvider>();
             container.RegisterInstance(typeof(IStorageContainer), null, new StorageContainer(storageProvider));
 
-            var original = ViewLocator.LocateForModelType;
-            ViewLocator.LocateForModelType += (o, bindableObject, arg3) =>
-            {
-                var view = original(o, bindableObject, arg3);
-                var label = view as Label;
-                if (label != null)
-                {
-                    // on all platforms Caliburn.Micro will display an error message when view resolve failed
-                    // but on xamarin forms this will crash because they don't return a proper page but only a label instead
-                    throw new NotSupportedException("Caliburn.Micro failed to resolve the view, here's the details: " + label.Text);
-                }
-                return view;
-            };
-
             DisplayRootView<RootView>();
         }
 
