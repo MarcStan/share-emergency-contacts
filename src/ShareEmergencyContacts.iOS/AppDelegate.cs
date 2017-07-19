16ca383fa3f5b747fb94dd9d7400452bcad07a68
MarcStan <marcstan@live.com>
2017-07-19 13:22:40 +0200

    added multi res icons for iOS

--- src/ShareEmergencyContacts.iOS/AppDelegate.cs
+++ src/ShareEmergencyContacts.iOS/AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using Foundation;
 using UIKit;
+using Xamarin.Forms.Platform.iOS;
 
 namespace ShareEmergencyContacts.iOS
 {
@@ -8,7 +9,7 @@ namespace ShareEmergencyContacts.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public class AppDelegate : FormsApplicationDelegate
     {
 
         private readonly CaliburnAppDelegate _caliburn = new CaliburnAppDelegate();
@@ -22,7 +23,7 @@ namespace ShareEmergencyContacts.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+            Xamarin.Forms.Forms.Init();
             LoadApplication(IoC.Get<App>());
 
             ZXing.Net.Mobile.Forms.iOS.Platform.Init();
