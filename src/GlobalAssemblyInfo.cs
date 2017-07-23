3d701ba39da2352b752a6a00e4b0c5774583c457
MarcStan <marcstan@live.com>
2017-07-24 01:20:15 +0200

    updated version

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
 [assembly: AssemblyCompany("MarcStan")]
 [assembly: AssemblyCopyright("Copyright © MarcStan 2017")]
