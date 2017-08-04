89685874453fe96c234674e81072e070e6cce4b5
MarcStan <marcstan@live.com>
2017-08-04 20:08:21 +0200

    now loading barcode via code behind so exception can be caught (e.g. if content too big)

--- src/ShareEmergencyContacts/Views/ProfileVisualizerView.xaml.cs
+++ src/ShareEmergencyContacts/Views/ProfileVisualizerView.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using ShareEmergencyContacts.ViewModels;
 using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
 
 namespace ShareEmergencyContacts.Views
 {
@@ -10,6 +14,46 @@ namespace ShareEmergencyContacts.Views
             InitializeComponent();
             BindingContextChanged += OnBindingContextChanged;
             CurrentPageChanged += OnPageChanged;
+
+            Share.Content = new ActivityIndicator
+            {
+                IsEnabled = true,
+                IsRunning = true,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            // setup barcode in background so view can load right away
+            Task.Run(() =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    try
+                    {
+                        // can't define in xaml as it throws exception if the barcode doesn't match (too big, wrong content, etc).
+                        var barcode = new ZXingBarcodeImageView
+                        {
+                            BarcodeFormat = BarcodeFormat.QR_CODE,
+                            HorizontalOptions = LayoutOptions.FillAndExpand,
+                            VerticalOptions = LayoutOptions.FillAndExpand,
+                        };
+                        barcode.SetBinding(ZXingBarcodeImageView.BarcodeValueProperty,
+                            new Binding("BarcodeContent", BindingMode.OneWay));
+                        barcode.SetBinding(ZXingBarcodeImageView.BarcodeOptionsProperty,
+                            new Binding("Options", BindingMode.OneWay));
+                        Share.Content = barcode;
+                    }
+                    catch (Exception)
+                    {
+                        // most likely due to barcode too big
+                        Share.Content = new Label
+                        {
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                            Text = "Failed to create QR code from profile. Probably too much data."
+                        };
+                    }
+                });
+            });
         }
 
         private void OnBindingContextChanged(object sender, EventArgs e)
