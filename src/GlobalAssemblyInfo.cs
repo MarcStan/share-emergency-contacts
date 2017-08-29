b45538c0c26762c6ead1dd3306e4290c7297f665
MarcStan <marcstan@live.com>
2017-08-29 19:33:55 +0200

    version bump

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.3")]
-[assembly: AssemblyFileVersion("0.9.3")]
+[assembly: AssemblyVersion("0.9.4")]
+[assembly: AssemblyFileVersion("0.9.4")]
 [assembly: AssemblyCompany("MarcStan")]
 [assembly: AssemblyCopyright("Copyright © MarcStan 2017")]
